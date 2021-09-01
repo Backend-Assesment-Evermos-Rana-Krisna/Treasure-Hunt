ced8c3fdabacf0bbbf4f6f4ee6f2739c4f3176c2
Rana Krisna <ranakrisna17031995@gmail.com>
2021-09-02 00:56:28 +0700

    Remove Comment

--- TreasureHunt.php
+++ TreasureHunt.php
@@ -8,9 +8,7 @@
             ["#",".","#",".",".",".",".","#"],
             ["#","#","#","#","#","#","#","#"]
         ];
-        private $clearPath = [];
-        private $position, $treasure;
-        private $found = false;
+        private $clearPath = [], $position, $treasure, $found = false;
         
         function __construct(array $options = [])
         {
@@ -44,56 +42,25 @@
 
         public function startHunt()
         {
-            /**
-             * BEGIN navigate by user from starting position by order
-             * Up/North = A step(s)
-             * Right/East = B step(s)
-             * Down/South = C step(s)
-             * note: 
-             * - if there is an obstacle, then position coord same as the last navigate position  
-             * - if navigatte user input is not number, it will turn the values to be 0
-             */ 
             echo PHP_EOL."===============".PHP_EOL;
-            
             echo "Navigate position (step): ".PHP_EOL;
-
-            // navigate how many step for Up/North
             $up = $this->_readInput('Up/North');
-            // navigate how many step for Right/East
             $right = $this->_readInput('Right/East');
-            // navigate how many step for Down/South
             $down = $this->_readInput('Down/South');
 
-            /**
-             * END navigate by user from starting position by order
-             */ 
-
-            // // // generate probable coord points where the treasure might be localted
-            // echo PHP_EOL."===============".PHP_EOL;
-            // echo "Probable Treasure Locations: ".PHP_EOL.$this->_setProbableTreasureCoord($up, $right, $down);
-            
-            // // generate grid after navigate position by user
-            // echo PHP_EOL."===============".PHP_EOL;
-            // $this->_generateGrid();
             echo PHP_EOL."============================".PHP_EOL;
             echo "Probable Treasure Locations: ".PHP_EOL.$this->_setProbableTreasureCoord($up, $right, $down).PHP_EOL;
 
-            // generate grid after navigate position by user
-            echo PHP_EOL."============================".PHP_EOL;
             if ($this->found == true) {
-                echo "Wohooo!!! Congratulations, You found the treasure...".PHP_EOL;
+                echo PHP_EOL."Wohooo!!! Congratulations, You found the treasure...".PHP_EOL;
             } else {
-                echo "Sorry, You have not made it, please try again...".PHP_EOL;
+                echo PHP_EOL."Sorry, You have not made it, please try again...".PHP_EOL;
             }
             echo $this->_generateGrid();
         }
 
-        /**
-         * change grid value coord according user navigate values
-         * @param int $val the value from user navigate
-         * @param string $navigate the direction from user navigate 
-         */
-        private function _setCoordValuesByUserNavigate($val = 0, $navigate = "up") {
+        private function _setCoordValuesByUserNavigate($val = 0, $navigate = "up") 
+        {
             while($val != 0) {
                 switch($navigate) {
                     case "right":
@@ -106,7 +73,6 @@
                                 $this->coord[$this->position->row][$currColumn] = "\e[0;31mX\e[0m";
                                 $this->position->column = $currColumn;
 
-                                // delete coord from clearPath
                                 if(array_key_exists($this->position->row."::".$currColumn, $this->clearPath)) {
                                     unset($this->clearPath[$this->position->row."::".$currColumn]);
                                 }
@@ -120,13 +86,11 @@
                         if($this->position->column == $this->treasure->column && $currRow == $this->treasure->row){
                             $this->found = true;
                         }
-                        
                         if($currRow >= 0) {
                             if($this->coord[$currRow][$this->position->column] == ".") {
                                 $this->coord[$currRow][$this->position->column] = "\e[0;31mX\e[0m";
                                 $this->position->row = $currRow;
 
-                                // delete coord from clearPath
                                 if(array_key_exists($currRow."::".$this->position->column, $this->clearPath)) {
                                     unset($this->clearPath[$currRow."::".$this->position->column]);
                                 }
@@ -137,19 +101,13 @@
                 $val--;
             }
         }
-        
-        /**
-         * generate probable treasure coord point values from user navigate values
-         * @param int $up
-         * @param int $right
-         * @param int $down
-         */
-        private function _setProbableTreasureCoord($up = 0, $right = 0, $down = 0) {
+
+        private function _setProbableTreasureCoord($up = 0, $right = 0, $down = 0) 
+        {
             if($up) $this->_setCoordValuesByUserNavigate($up, "up");
             if($right) $this->_setCoordValuesByUserNavigate($right, "right");
             if($down) $this->_setCoordValuesByUserNavigate($down, "down");
 
-            // get list of probable treasure coord from clearPath after user navigate process 
             $listTreasureCoord = [];
             foreach($this->clearPath as $key => $coord) {
                 $listTreasureCoord[] = "({$coord->row}, {$coord->column})";
@@ -159,9 +117,6 @@
             return implode(PHP_EOL, $listTreasureCoord);
         }
 
-        /**
-         * generate grid for grid coord values
-         */
         private function _generateGrid()
         {
             for ($x = 0; $x < count($this->coord); $x++) {
